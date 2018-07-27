45fd8a71e717f195dca19800bf927dd6be983e4f
Jordan Hudgens <jordan@devcamp.com>
2018-07-27 10:50:47 -0600

    built signup form

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 
+import SignupForm from "./signupForm";
+
 class Signup extends Component {
   render() {
-    return <div className="sign-up">...signup</div>;
+    return (
+      <div className="sign-up">
+        <SignupForm />
+      </div>
+    );
   }
 }
 
--- src/components/formTitle.js
+++ src/components/formTitle.js
@@ -2,5 +2,5 @@ import React from "react";
 
 export function FormTitle(props) {
   const { className, text } = props;
-  return <h1 className={`${className}f form-title`}> {text} </h1>;
+  return <h1 className={`${className} form-title`}> {text} </h1>;
 }
